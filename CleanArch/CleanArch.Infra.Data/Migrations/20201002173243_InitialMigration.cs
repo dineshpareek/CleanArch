b187514de90bce3ec85680aa8a321074cda05c43
Dinesh Pareek <Dinesh.pareek@live.com>
2020-10-02 23:11:05 +0530

    Initial Commit for Infra.Data layer

---
+++ CleanArch/CleanArch.Infra.Data/Migrations/20201002173243_InitialMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArch.Infra.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    CourseId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseName = table.Column<string>(nullable: true),
+                    CourseImg = table.Column<string>(nullable: true),
+                    CourseCode = table.Column<string>(nullable: true),
+                    Credit = table.Column<float>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.CourseId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
